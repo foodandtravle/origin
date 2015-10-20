83a5f7d3275cf5c07742606e5f03d8eb3bfb0c5b
张德强 <952528232@qq.com>
2015-10-20 14:16:55 +0800

    创建一个AVPlayerDemo

---
+++ AVPlayer/AVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by 张德强 on 15/10/20.
+//  Copyright © 2015年 张德强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
